a860623ccd2285fa14154916ec3251ad705d0891
Lance <contact@lefside.com>
2023-04-18 20:31:20 -0400

    Added Player Movement

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ class Sprite {
   constructor({position, velocity}) {
     this.position = position;
     this.velocity = velocity;
-<<<<<<< HEAD
+
     this.height = 150
-=======
->>>>>>> f4ba0a88518935c4090a261f47d9a0029371d0d2
+
   }
 
   draw() {
@@ -27,19 +26,16 @@ class Sprite {
 
   update() {
     this.draw()
-    this.velocity.y += gravity
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
 
      if(this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
-     }
+     }else this.velocity.y += gravity
+     
 
   }
 
-  update() {
-    this.draw()
-    this.position.y += 10
-  }
 }
 
 const player = new Sprite({
@@ -70,15 +66,73 @@ enemy.draw();
 
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  }
+  
+}
+
+let lastKey
+
 function animate() {
   window.requestAnimationFrame(animate);
-<<<<<<< HEAD
+
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height)
-=======
->>>>>>> f4ba0a88518935c4090a261f47d9a0029371d0d2
+
   player.update()
   enemy.update()
+
+
+   player.velocity.x = 0;
+
+   if (keys.a.pressed && lastKey === 'a') {
+    player.velocity.x = -1
+   } else if (keys.d.pressed && lastKey ==='d') {
+    player.velocity.x = 1
+   }
+
 }
 
 animate();
+
+
+window.addEventListener('keydown', (event) => {
+  switch(event.key) {
+    case 'd':
+      keys.d.pressed = true;
+      lastKey = 'd'
+      break
+      case 'a':
+     keys.a.pressed = true;
+     lastKey = 'a'
+      break
+      case 'w':
+     player.velocity.y = -10
+      break
+  }
+  console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+  switch(event.key) {
+    case 'd':
+     keys.d.pressed = false;
+      break
+      case 'a':
+      keys.a.pressed = false;
+      break
+      case 'w':
+     keys.w.pressed = false;
+     lastKey = 'w'
+      break
+  }
+  console.log(event.key);
+})
