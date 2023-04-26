8ad6f5a3bdb25297c3be930e792b3d0c06b3a4a2
Lance <contact@lefside.com>
2023-04-26 01:01:18 -0400

    Updated

--- js/index.js
+++ js/index.js
@@ -18,53 +18,6 @@ const background = new Sprite({
  
 })
 
-<<<<<<< HEAD:js/index.js
-=======
-class Sprite {
-  constructor({position, velocity, color = 'red', offset}) {
-    this.position = position;
-    this.velocity = velocity;
-    this.lastKey
-    this.width = 50
-    this.height = 150
-    this.attackBox = {
-      position: {
-      x:this.position.x ,
-      y:this.position.y
-    },
-    offset,
-      width: 100,
-      height: 50  
-    },     
-   this.color = color
-   this.isAttacking
-   this.health= 100
-  }
-
-  draw() {
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, 50, this.height, this.width);
-
-    //Attack Box
-    if(this.isAttacking) {
-    c.fillStyle= "green";
-    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-    }
-  }
-
-  update() {
-    this.draw()
-    this.attackBox.position.x = this.position.x - this.attackBox.offset.x
-    this.attackBox.position.y = this.position.y
-
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-     if(this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0;
-     }else this.velocity.y += gravity
-     
->>>>>>> 198453be14ea20cc488453744a43747067d11257:index.js
 
 const shop = new Sprite({
   position: {
@@ -93,7 +46,6 @@ velocity:{
 offset: {
   x:0,
   y:0
-<<<<<<< HEAD:js/index.js
 },
 imageSrc: './Martial Hero/Sprites/Idle.png',
 framesMax: 8,
@@ -111,8 +63,6 @@ sprites: {
     imageSrc: './Martial Hero/Sprites/Run.png',
     framesMax: 8
   }
-=======
->>>>>>> 198453be14ea20cc488453744a43747067d11257:index.js
 }
 });
 
@@ -161,15 +111,6 @@ const keys = {
 
 
 decreaseTimer();
-
-function rectangularCollision({rectangle1,rectangle2}) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
-    && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-    && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  )
-}
-
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -230,7 +171,6 @@ function animate() {
  if (enemy.health <= 0 || player.health <= 0){
 determineWinner({player, enemy, timerId})
  }
-
 
 
 }
